171a66df9fa1c28c2c6551a7162fb6b5d8a751f0
Rohit Goswami <rog32@hi.is>
2025-06-02 09:02:14 +0200

    MAINT: Drop suffix usage
    Co-authored-by: luthaf <luthaf@users.noreply.github.com>

--- plumed-metatomic/build.sh
+++ plumed-metatomic/build.sh
@@ -32,7 +32,7 @@ if [[ $(uname) == "Linux" ]]; then
 fi
 
 # we also store path so that software linking libplumedWrapper.a knows where libplumedKernel can be found.
-export CPPFLAGS="-D__PLUMED_DEFAULT_KERNEL=$PREFIX/lib/libplumed${PSUFFIX}Kernel$SHLIB_EXT $CPPFLAGS"
+export CPPFLAGS="-D__PLUMED_DEFAULT_KERNEL=$PREFIX/lib/libplumedKernel$SHLIB_EXT $CPPFLAGS"
 
 # enable optimization
 export CXXFLAGS="${CXXFLAGS//-O2/-O3}"
@@ -53,7 +53,6 @@ export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 ./configure --prefix="$PREFIX" \
             --disable-python \
             "$MPI_FLAG" \
-            --program-suffix="$PSUFFIX" \
             --disable-libsearch \
             --disable-static-patch \
             --disable-static-archive \
